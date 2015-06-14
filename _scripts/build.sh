a5b88641ee1b19ea047833e7ecfc251042b37f4a
Kosta Harlan <kosta@kostaharlan.net>
2015-06-14 12:34:31 -0400

    Update schedule

---
+++ _scripts/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# TODO: Use curl/wget to download the CSV.
+# TODO: Get dates programatically
+cat /tmp/futsal.csv | csvgrep -c 1 -r "6/15/2015|6/16/2015" | csv2json > _data/2015/summer/week.json
+cat /tmp/futsal.csv | csvgrep -i -c 1 -r "6/15/2015|6/16/2015" | csv2json > _data/2015/summer/schedule.json
+cat /tmp/futsal.csv | csv2json > _data/2015/summer/matches.json
