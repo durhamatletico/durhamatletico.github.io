8a5e73409fc1da5c94ad75a6fa19854c8fdc06bb
Kosta Harlan <kosta@kostaharlan.net>
2015-06-24 15:20:29 -0400

    Update stats

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # TODO: Use curl/wget to download the CSV.
 # TODO: Get dates programatically
-cat /tmp/futsal.csv | csvgrep -c 1 -r "6/22/2015|6/23/2015" | csv2json > _data/2015/summer/week.json
-cat /tmp/futsal.csv | csvgrep -i -c 1 -r "6/15/2015|6/16/2015|6/22/2015|6/23/2015" | csv2json > _data/2015/summer/schedule.json
-cat /tmp/futsal.csv | csvgrep -c 1 -r "6/15/2015|6/16/2015" | csv2json > _data/2015/summer/matches.json
+# Set dates for upcoming week
+cat /tmp/futsal.csv | csvgrep -c 1 -r "6/29/2015|6/30/2015" | csv2json > _data/2015/summer/week.json
+# Set schedule for beyond upcoming week
+cat /tmp/futsal.csv | csvgrep -i -c 1 -r "6/15/2015|6/16/2015|6/22/2015|6/23/2015|6/29/2015|6/30/2015" | csv2json > _data/2015/summer/schedule.json
+# Get results for processing
+cat /tmp/futsal.csv | csvgrep -c 1 -r "6/15/2015|6/16/2015|6/22/2015|6/23/2015" | csv2json > _data/2015/summer/matches.json
 python _scripts/process.py
