5380f8840f39527317117f97e28b773ed2a79bf3
Kosta Harlan <kosta@kostaharlan.net>
2015-07-08 10:03:21 -0400

    Update schedule and standings

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -2,11 +2,11 @@
 # TODO: Use curl/wget to download the CSV.
 # TODO: Get dates programatically
 # Set dates for upcoming week
-cat /tmp/futsal.csv | csvgrep -c 1 -r "7/6/2015|7/7/2015" | csv2json > _data/2015/summer/week.json
+cat /tmp/futsal.csv | csvgrep -c 1 -r "7/13/2015|7/14/2015" | csv2json > _data/2015/summer/week.json
 # Set schedule for beyond upcoming week
-cat /tmp/futsal.csv | csvgrep -i -c 1 -r "6/15/2015|6/16/2015|6/22/2015|6/23/2015|6/29/2015|6/30/2015" | csv2json > _data/2015/summer/schedule.json
+cat /tmp/futsal.csv | csvgrep -i -c 1 -r "6/15/2015|6/16/2015|6/22/2015|6/23/2015|6/29/2015|6/30/2015|7/6/2015|7/7/2015" | csv2json > _data/2015/summer/schedule.json
 # Get previous games
-cat /tmp/futsal.csv | csvgrep -c 1 -r "6/15/2015|6/16/2015|6/22/2015|6/23/2015|6/29/2015|6/30/2015" | csv2json > _data/2015/summer/past.json
+cat /tmp/futsal.csv | csvgrep -c 1 -r "6/15/2015|6/16/2015|6/22/2015|6/23/2015|6/29/2015|6/30/2015|7/6/2015|7/7/2015" | csv2json > _data/2015/summer/past.json
 # Get results for processing
-cat /tmp/futsal.csv | csvgrep -c 1 -r "6/15/2015|6/16/2015|6/22/2015|6/23/2015|6/29/2015|6/30/2015" | csv2json > _data/2015/summer/matches.json
+cat /tmp/futsal.csv | csvgrep -c 1 -r "6/15/2015|6/16/2015|6/22/2015|6/23/2015|6/29/2015|6/30/2015|7/6/2015|7/7/2015" | csv2json > _data/2015/summer/matches.json
 python _scripts/process.py
